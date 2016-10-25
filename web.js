9331019a5b8cd4230b30b58b1dba0771616e9024
Glenn X. Wright <glenn.t.wright@kp.org>
2016-10-25 14:59:11 -0700

    would that this works

--- web.js
+++ web.js
@@ -11,15 +11,15 @@ var app = express();
 
 app.get('/', function(request, response) {
     //connection.query("create table test (fname varchar(20), lname varchar(20));");
-    connection.query("insert into test values('Glenn','Wright');");
-    //connection.query("select * from test", function(err, rows, fields) {
-    //  if (err) {
-    //    console.log("error: ", err);
-    //    throw err;
-    //  }
-    //  response.send("Hello world!", rows);
-    //});
-    response.send(connection.state);
+    //connection.query("insert into test values('Glenn','Wright');");
+    connection.query("select * from test", function(err, rows, fields) {
+      if (err) {
+        console.log("error: ", err);
+        throw err;
+      }
+      response.send("Hello world!", rows);
+    });
+    //response.send(connection.state);
     //response.send('Hello World!!!! HOLA MUNDO!!!!');
 });
 
