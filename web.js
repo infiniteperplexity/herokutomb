a666c6bc1ca4e2275fa347ab25d60d219887bd4a
Glenn X. Wright <glenn.t.wright@kp.org>
2016-10-25 12:45:41 -0700

    let's commit some stuff

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 var express = require("express");
 var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'us-cdbr-iron-east-04.cleardb.net',
+  user: 'bc8309dedbcac6',
+  password: '5271b38e',
+  database: 'heroku_d9a408c946dbc65'
+});
+connection.connect();
 var app = express();
-
-var db_config = {
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'b6d6c6e8740d20',
-    password: 'b3f75ada',
-    database: 'heroku_1daa39da0375291'
-};
-
-var connection;
 
 app.get('/', function(request, response) {
     response.send('Hello World!!!! HOLA MUNDO!!!!');
