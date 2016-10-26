2648277c571765382ae2205f6dffeb4374980b53
Glenn X. Wright <glenn.t.wright@kp.org>
2016-10-26 13:57:40 -0700

    roll back the last thing

--- web.js
+++ web.js
@@ -12,8 +12,7 @@ var app = express();
 app.get('/', function(request, response) {
     //connection.query("create table test (fname varchar(20), lname varchar(20));");
     //connection.query("insert into test values('Glenn','Wright');");
-    //connection.query("select* from test", function(err, rows, fields) {
-    connection.query("show tables", function(err, rows, fields) {
+    connection.query("select* from test", function(err, rows, fields) {
       if (err) {
         console.log("error: ", err);
         throw err;
