9d742a5b629357ff7f39cc9c30dbb581bd574407
Glenn X. Wright <glenn.t.wright@kp.org>
2016-10-28 11:51:42 -0700

    cleaned nulls from save game list

--- web.js
+++ web.js
@@ -69,6 +69,13 @@ app.get('/saves/', function(req, res) {
       res.send(" ");
     } else {
       rows = rows.map(function(a,e,i) {return e.filename;});
+      // clean the array
+      for (let i=0; i<rows.length; i++) {
+        if (rows[i]===null || rows[i]===undefined) {
+          this.splice(i,1);
+          i--;
+        }
+      }
       res.send(JSON.stringify(rows));
     }
   });
