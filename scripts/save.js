c534b253112acfc256b86d74735e007e317b9828
Glenn X. Wright <glenn.t.wright@kp.org>
2016-11-04 14:55:32 -0700

    allowed for undefined response

--- scripts/save.js
+++ scripts/save.js
@@ -449,7 +449,7 @@ HTomb = (function(HTomb) {
     Promise.all(promises).then(
       values => {
         for (let i=0; i<values.length; i++) {
-          if (values[i].ok===false) {
+          if (!values[i] || values[i].ok===false) {
             console.log("response for " + values[i].url + " not ok");
             HTomb.Time.unlockTime();
             HTomb.GUI.Contexts.locked=false;
