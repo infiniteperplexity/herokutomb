6060f195c407fade14e4396903a983923ad047ae
Glenn X. Wright <glenn.t.wright@kp.org>
2016-11-04 13:44:20 -0700

    fixed a typo

--- scripts/types.js
+++ scripts/types.js
@@ -50,7 +50,7 @@ HTomb = (function(HTomb) {
       };
       // Create a parsing function for the type
       HTomb.Types["parse" + args.template] = function(value) {
-        if (typeof(value==="number")) {
+        if (typeof(value)==="number") {
           return HTomb.Types.templates[args.template].types[value];
         } else {
           return value;
