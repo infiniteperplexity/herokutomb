b5e4a8c150b1bdcb5d53452acff1dc41f4e1910e
Glenn X. Wright <glenn.t.wright@kp.org>
2016-12-28 14:43:52 -0800

    did i fix it this time?

--- scripts/ai.js
+++ scripts/ai.js
@@ -38,7 +38,7 @@ HTomb = (function(HTomb) {
       //  return false;
       //}
       // check to see if we are already targeting an ingredient
-      if (cr.ai.target.isPlaced()!==true) {
+      if (t && cr.ai.target.isPlaced()!==true) {
         cr.ai.target = null;
       }
       var t = cr.ai.target;
