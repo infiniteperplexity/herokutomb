89e34e29897ff28b275d8e3ec2b684bad23b1c0d
Glenn X. Wright <glenn.t.wright@kp.org>
2017-01-18 15:51:33 -0800

    fixed loop

--- devscripts/save.js
+++ devscripts/save.js
@@ -355,15 +355,15 @@ HTomb = (function(HTomb) {
       if (thing.task) {
         HTomb.World.tasks[coord(x,y,z)]=thing;
       }
-      console.time("validate behaviors");
-      for (let i=0; i<HTomb.World.things.length; i++) {
-        let thing = HTomb.World.things[i];
-        if (thing.Behaviors) {
-          thing.validateBehaviors();
-        }
-      }
-      console.timeEnd("validate behaviors");
     }
+    console.time("validate behaviors");
+    for (let i=0; i<HTomb.World.things.length; i++) {
+      let thing = HTomb.World.things[i];
+      if (thing.Behaviors) {
+        thing.validateBehaviors();
+      }
+    }
+    console.timeEnd("validate behaviors");
   }
 
   function restoreTiles(z1,z2) {
